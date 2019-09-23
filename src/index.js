a26b66836cfe8f80e76067ab9e11d997071b1887
suntao <suntao@DESKTOP-P8IU6JF>
2019-09-23 10:00:42 +0800

    read_template

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import './assets/css/reset.css'
 import './assets/css/style.scss'
 import './assets/fonts/iconfont.css'
 import {a} from './assets/js/base'
-import $ from 'jquery'
 
 // import './App'/
 console.log("webpack fdsfdsf打包更新");
-var num = jQuery('.box-big').length;
+var num = jQuery('#box-big').length;
 console.log("box-big:"+num)
 
 console.log(a());
